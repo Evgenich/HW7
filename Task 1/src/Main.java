27d68feb43d35f9baf01ee462a4d7fb58ab918d8
Evgenich <Yanevgenevich@gmail.com>
2023-02-16 22:53:15 +0300

    написан код

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int toadEyesCount = 0;          //Жабьи глаза
         int ghoulTearsCount = 0;        //Слезы вурдалака
         int ravenBonesCount = 0;        //Кости ворона
         int dumplingsCount = 0;         //Пельмени(а почему, собственно нет? xD )
+        boolean isNotCook=false;
 
         //todo реализовать ввод пользователем кол-ва ингредиентов.
+        System.out.println("Введите кол-во жабьих глаз:");
+        toadEyesCount = new Scanner(System.in).nextInt();
+
+        System.out.println("Введите кол-во слезы вурдалака:");
+        ghoulTearsCount = new Scanner(System.in).nextInt();
+
+        System.out.println("Введите кол-во костей ворона:");
+        ravenBonesCount = new Scanner(System.in).nextInt();
+
+        System.out.println("Введите кол-во пелмений:");
+        dumplingsCount = new Scanner(System.in).nextInt();
+
 
         //Рецепты Эликсиров:
         // 1. 3 Жабьих глаза + 1 кость ворона - Эликсир зоркости
@@ -14,5 +29,35 @@ public class Main {
         // 4. 5 слез вурдалака + 10 пельменей + 4 жабьих глаза + 3 кости ворона - Запретный эликсир
 
         //todo дописать логику программы сюда
+        if (toadEyesCount >=3 && ravenBonesCount >=1)
+        {
+            System.out.println("Вы можете изготовить:\n" + "Элексир зоркости");
+            isNotCook=true;
+        }
+            if (ravenBonesCount>=2&&dumplingsCount >=4)
+            {
+            System.out.println("Вы можете изготовить:\n" + "Элексир стойкости");
+            isNotCook=true;
+            }
+
+        if (ghoulTearsCount>=7&&dumplingsCount >=1&&toadEyesCount >=2)
+        {
+            System.out.println("Вы можете изготовить:\n" + "Элексир скрытности");
+            isNotCook=true;
+        }
+
+        if (ghoulTearsCount>=5&&dumplingsCount >=10&&toadEyesCount >=4&&ravenBonesCount >=3)
+        {
+            System.out.println("Вы можете изготовить:\n" + "Элексир Запретный");
+            isNotCook=true;
+        }
+
+        if (!isNotCook)
+        {
+            System.out.println("не хватает ингридиентов");
+        }
+
     }
+
 }
+
--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
+
+//                for (int i = 0 ; i < 10; i++) {
+//            if (i<3 || i>7){
+//                continue;
+//            }
+//            System.out.println("Hello");
+//        }
+
+//        for (int i=2;i<=100;i=i+2)
+//        System.out.println("уволен айтишник с id"+i);
+
+        int i = 2;
+        while (i <=100) {
+            System.out.println("уволен айтишник с id "  + i);
+            i+=2;
+        }
+
+
+
+
+
     }
 }
